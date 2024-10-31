ccdeeccfbb4d437c7a13f3102a5b3118acd52719
Mojtaba <mojtabakhedri1924@gmail.com>
2024-10-31 17:27:39 +0330

    3 ways to comment in php with vscode

---
+++ 001-How-write-comment/index.php
@@ -0,0 +1,20 @@
+<?php
+// comments are important in coding because they guide us in future or guide our colleagues
+// there's three ways to write comments in php
+// first way to comment in php is using two / (The slash sign) frist of line
+// like example below
+// echo ("Hello World!")
+// When we use this signs first of a line ; That line won't perform
+// second way to comment is putting one # first of the line like below examole
+# echo ("I love Coding")
+// and third way that is more common is to select the code that you want to comment and hit ctrl + / (maybe just for vscode)
+// echo ("Hello World!")
+// echo ("Hello World!")
+// echo ("Hello World!")
+// echo ("Hello World!")
+// echo ("Hello World!")
+// echo ("Hello World!")
+// echo ("Hello World!")
+// echo ("Hello World!")
+// That's it.
+
